0590196638a6a2b1bb5b4bf5fbaed2429751b182
fluffypill0w <whitemeganalicia@protonmail.com>
2021-05-23 17:55:18 +0200

    Miguel you're our only hope

--- test/zkurrate.circom
+++ test/zkurrate.circom
@@ -5,19 +5,29 @@ include "../../node_modules/circomlib/circuits/bitify.circom"; // TODO: remove?
 //var pubEmployerNameSignalSize = 16*8;
 
 template StringIsHello() {
-    signal input inArray[1];
-    signal output isHello;
+    signal input inArray[2];
+    //signal output isHello;
+    signal output z;
     var n = 8;    
     
     component firstByteBits = Num2Bits(n);
-    firstByteBits.in <-- inArray[0];    
+    firstByteBits.in <== inArray[0];    
     var hChar = 104;
 
     component hBits = Num2Bits(n);
-    hBits.in <-- hChar;    
+    hBits.in <== hChar;    
+
+    component secondByteBits = Num2Bits(n);
+    secondByteBits.in <== inArray[1];  
+    var iChar = 105;
+    z <== 106;
+
+    component iBits = Num2Bits(n);
+    iBits.in <== iChar;  
     
     for (var i=0; i<n; i++) {
         firstByteBits.out[i] === hBits.out[i];
+        secondByteBits.out[i] === iBits.out[i];
     }
 }
 
--- test/zkurrate_test.js
+++ test/zkurrate_test.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const tester = require("circom").tester;
 
-//const Fr = require("ffjavascript").bn128.Fr;
+const bn128 = require("ffjavascript").bn128;
 
 const assert = chai.assert;
 
@@ -11,15 +11,18 @@ describe("My test", function () {
 
     this.timeout(100000000);
 
-    it("Should equal true", async () => {
+    it("should give 'h' followed by 'i'", async () => {
         assert(true);
 
         const circuit = await tester(path.join(__dirname, "", "zkurrate.circom"));
         await circuit.loadConstraints();
 
-        assert.equal(circuit.nVars, 10);
-        assert.equal(circuit.constraints.length, 17);
+        assert.equal(circuit.nVars, 18);
+        assert.equal(circuit.constraints.length, 34);
 
-        const witness = await circuit.calculateWitness({ "inArray": "104"}, true);
+        const witness = await circuit.calculateWitness({ "inArray": ["104", "105"]}, true);
+
+        assert(bn128.eq(witness[0],bn128.e(1)));
+       // assert(witness[1] == "105");
     });
 });
