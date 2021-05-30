2b5c8c1170a5bc9073b19c1dc8fe4ac276a52d3c
fluffypill0w <whitemeganalicia@protonmail.com>
2021-05-30 23:01:43 +0200

    Removed unneeded files

--- app/backend/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Zkurrate
-
-class Zkurrate(admin.ModelAdmin):
-    list_display = ('title', 'description', 'completed')
-
-# Register models
-admin.site.register(Zkurrate)
